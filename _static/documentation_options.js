2a442c73cbd8ed2fcb0c442b5322c83efb93aec7
jkotilahti <83180458+jkotilahti@users.noreply.github.com>
2024-09-27 15:54:01 +0000

    Deploying to gh-pages from @ jkotilahti/iqm-client@5de3df26d32de6e7bf82a9b8a765bf5b894c53dc 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev367+g332531e',
+    VERSION: '0.0.post1.dev368+g5de3df2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
